dde38dc73be253aea0d0c96893e1415e4eb868fb
Allen Korenevsky <theplatapi@gmail.com>
2013-10-17 18:22:57 -0700

    Initial push. Just including some comments on how to structure the JDBC uri

---
+++ src/JDBCSample.java
@@ -0,0 +1,5 @@
+
+public class JDBCSample {
+    //ftmdb.cbeadsxspecl.us-west-2.rds.amazonaws.com:3306
+    //String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+}
