db962051c016d57b990742128641c5d2439ff6dc
Allen Korenevsky <theplatapi@gmail.com>
2013-10-22 14:51:11 -0700

    Added basic connection and execute methods. Does not return a result set yet.

--- src/JDBCSample.java
+++ src/JDBCSample.java
@@ -1,5 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCSample {
-    //ftmdb.cbeadsxspecl.us-west-2.rds.amazonaws.com:3306
-    //String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+    private Connection connection;
+
+    private String jdbcURL;
+    private StringBuilder jdbcURLBuilder = new StringBuilder(100);
+    private String hostname = "ftmdb.cbeadsxspecl.us-west-2.rds.amazonaws.com";
+    private String port = "3306";
+    private String dbName = "Stocks";
+    private String username = "ftm";
+    private String password = "ftm-pass";
+
+    public JDBCSample() {
+        //String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+        jdbcURLBuilder.append("jdbc:mysql://");
+        jdbcURLBuilder.append(hostname);
+        jdbcURLBuilder.append(':');
+        jdbcURLBuilder.append(port);
+        jdbcURLBuilder.append('/');
+        jdbcURLBuilder.append(dbName);
+        jdbcURLBuilder.append("?user=");
+        jdbcURLBuilder.append(username);
+        jdbcURLBuilder.append("&password=");
+        jdbcURLBuilder.append(password);
+        jdbcURL = jdbcURLBuilder.toString();
+    }
+
+    public void connect() throws SQLException {
+        connection = DriverManager.getConnection(jdbcURL);
+    }
+
+    public void executeSQLStatement(String sqlStatement) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute(sqlStatement);
+    }
 }
