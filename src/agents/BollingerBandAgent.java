5982ee9615111834ebe9c737b6f90bff11810a5b
Allen Korenevsky <theplatapi@gmail.com>
2013-11-05 21:49:45 -0800

    Initial commit, just has default values saved

---
+++ src/agents/BollingerBandAgent.java
@@ -0,0 +1,11 @@
+package agents;
+
+/**
+ * User: allen
+ * Date: 11/5/13
+ * Time: 9:45 PM
+ */
+public class BollingerBandAgent {
+    private final int movingAverageSampleSize = 20;
+    private final int bandWidthInStandardDeviations = 2;
+}
