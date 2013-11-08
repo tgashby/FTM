b939cd891b06def12aebd683b780950b60a86947
Tag Ashby <tgashby@calpoly.edu>
2013-11-07 17:09:09 -0800

    Forgot to uncomment.

--- src/YFQuerier/YFQuerier.java
+++ src/YFQuerier/YFQuerier.java
@@ -87,7 +87,7 @@ public class YFQuerier extends TimerTask {
         exchangeClose.setTimeZone(TimeZone.getTimeZone("EST"));
         exchangeClose.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_WEEK), 4, 0);
 
-//        if (cal.after(exchangeOpen) && cal.before(exchangeClose))
+        if (cal.after(exchangeOpen) && cal.before(exchangeClose))
         {
             try {
                 URL yahooFinance = new URL("http://finance.yahoo.com/d/quotes.csv?s=" + urlOptions);
