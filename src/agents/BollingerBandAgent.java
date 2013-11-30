030a365a6e680eca916276bf06960ed4248ae43b
Allen Korenevsky <theplatapi@gmail.com>
2013-11-29 19:48:58 -0800

    consolidated files

--- src/agents/BollingerBandAgent.java
+++ src/agents/BollingerBandAgent.java
@@ -1,12 +1,11 @@
 package agents;
 
-import common.BasicStatistics;
 import common.Market;
 import common.Stock;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
 
 /**
@@ -136,4 +135,52 @@ public class BollingerBandAgent {
         }
         return wallet + portfolioWorth;
     }
+
+    private class BasicStatistics {
+        private ArrayBlockingQueue<Double> sample;
+        private double mean;
+        private double standardDeviation;
+
+        public BasicStatistics(int sampleSize) {
+            sample = new ArrayBlockingQueue<Double>(sampleSize);
+        }
+
+        public void add(double sampleValue) {
+            int oldSampleSize = sample.size();
+            sample.add(sampleValue);
+            int newSampleSize = sample.size();
+
+            //update mean
+            double sum = mean * oldSampleSize;
+            mean = (sum + sampleValue) / newSampleSize;
+        }
+
+        public void removeOldestValue() {
+            int oldSampleSize = sample.size();
+            double removedItem = sample.remove();
+            int newSampleSize = sample.size();
+
+            //update mean
+            double sum = mean * oldSampleSize;
+            mean = (sum - removedItem) / newSampleSize;
+        }
+
+        public double getMean() {
+            return mean;
+        }
+
+        public double getStandardDeviation() {
+            Iterator<Double> iterator = sample.iterator();
+            double mean = getMean();
+            double temp = 0;
+
+            while (iterator.hasNext()) {
+                double next = iterator.next();
+                temp += (mean - next) * (mean - next);
+            }
+
+            return Math.sqrt(temp / sample.size());
+        }
+    }
+
 }
