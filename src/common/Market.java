190e50e9f01754ede9eee17f06a1c0197faa8bac
Allen Korenevsky <theplatapi@gmail.com>
2013-11-29 21:36:30 -0800

    Market now trades any agent implementing the AgentInterface

--- src/common/Market.java
+++ src/common/Market.java
@@ -1,5 +1,8 @@
 package common;
 
+import agents.AgentInterface;
+import agents.BollingerBandAgent;
+
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -12,35 +15,40 @@ import java.util.ArrayList;
 public class Market {
     private ArrayList<StockValue> stockValues;
     private int stockNdx;
+    /**
+     * Use this to select different days from today in the getStocksByDay method
+     */
+    private final int millisecondsInADay = 86400000;
+    private ArrayList<AgentInterface> agents;
 
-    public Market(Date date) {
+    public Market() throws SQLException {
         DatabaseConnection databaseConnection = new DatabaseConnection();
 
         databaseConnection.connect();
-        stockValues = databaseConnection.getStocksByDay(date);
+        stockValues = databaseConnection.getStocksByDay(new Date(System.currentTimeMillis()));
         databaseConnection.disconnect();
 
-        stockNdx = 0;
+        /**
+         * refer to here for an explanation of this trick:
+         * http://stackoverflow.com/questions/924285/efficiency-of-java-double-brace-initialization
+         *
+         * Add an agent to the list so it gets invoked.
+         */
+        agents = new ArrayList<AgentInterface>()
+        {{
+            add(new BollingerBandAgent());
+        }};
     }
 
-    public StockValue getNextValue()
-    {
-        StockValue toReturn = null;
+    public void executeTrades() {
+        StockValue currentStockValue = stockValues.get(stockNdx++);
 
-        if (hasNextValue())
-        {
-            toReturn = stockValues.get(stockNdx);
-            stockNdx++;
-        }
-        else
-        {
-            throw new RuntimeException("No more stock values!");
-        }
-
-        return toReturn;
+        while (hasNextStockValue())
+            for (int i = 0; i < agents.size(); i++)
+                agents.get(i).trade(currentStockValue);
     }
 
-    public boolean hasNextValue() {
+    public boolean hasNextStockValue() {
         return stockNdx < stockValues.size();
     }
 }
